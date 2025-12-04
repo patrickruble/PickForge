315534613c699350149596129dfe271395941f7a
Patrick Ruble <98193219+patrickruble@users.noreply.github.com>
2025-12-04 17:31:25 -0600

    Add Vercel analytics tracking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,22 @@
-import './index.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { inject } from "@vercel/analytics";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode><App /></StrictMode>
+// Enable Vercel Analytics
+inject();
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-//if (process.env.NODE_ENV !== "production") {
-//  import("@axe-core/react").then(({ default: axe }) => {
- //   const React = require("react");
-  //  const ReactDOM = require("react-dom");
-  //  axe(React, ReactDOM, 1000);
- // });
-//}
+
+// if (process.env.NODE_ENV !== "production") {
+//   import("@axe-core/react").then(({ default: axe }) => {
+//     const React = require("react");
+//     const ReactDOM = require("react-dom");
+//     axe(React, ReactDOM, 1000);
+//   });
+// }
